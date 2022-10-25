dd97c79b28569004964a04e9fd8085f03928292f
martinwik <martinwik12@icloud.com>
2022-10-25 14:20:27 +0200

    Add project files.

---
+++ JsonTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Program
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+            //List<int> listNummer = JsonConvert.DeserializeObject<List<int>>(File.ReadAllText(@".\data.json"));
+        }
+    }
+}
